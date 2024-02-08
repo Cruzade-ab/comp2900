19470f4fedaf36737db1d318b8f682652bd4aa2d
Cruzade-ab <abimael.santa1@gmail.com>
2024-02-08 14:23:23 -0400

    lab04

---
+++ modulo001/lab04/app.py
@@ -0,0 +1,18 @@
+#For loop 
+print("For #1")
+for n in range(10):
+    print(n + 1)
+
+    
+print("For #2")
+for n in range(1,9):
+    print(n)
+
+
+#While loop
+print("For #3")
+n = 0 
+while (n<4): 
+    n = n+1
+    print(n)
+
