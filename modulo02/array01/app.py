98733da31319f42b57c04acdda9ada907bed0309
Cruzade-ab <abimael.santa1@gmail.com>
2024-02-29 14:24:12 -0400

    Array01

---
+++ modulo02/array01/app.py
@@ -0,0 +1,16 @@
+import array 
+aNumber = array.array('i')
+
+
+for i in range(1, 4):
+    number = int(input('Enter a number: '))
+    aNumber.append(number)
+
+for i in range(3):
+    print (f'Indice: {i} -Valor: {aNumber[i]}')
+    
+for n in aNumber:
+    print (f'Valor:{n}')
+    
+for i in range(len(aNumber)):
+    print(f"Valor {n}")
