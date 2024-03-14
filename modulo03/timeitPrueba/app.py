bcab12ee820373db27ab88151b3f36c6ad730759
Cruzade-ab <abimael.santa1@gmail.com>
2024-03-14 14:20:27 -0400

    ..

---
+++ modulo03/timeitPrueba/app.py
@@ -0,0 +1,28 @@
+import timeit
+
+def factorial_iterativo(n):
+    resultado = 1
+    for i in range(1, n + 1): 
+        resultado *= i
+    return resultado
+
+def factorial_recursivo(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial_recursivo(n-1)
+    
+#Tiempo del enfoque iterativo
+
+tiempo_iterativo = timeit.timeit('factorial_iterativo(10)', globals=globals(), number=100)
+tiempo_recursivo = timeit.timeit('factorial_recursivo(10)', globals=globals(), number=100)
+
+print(f"Tiempo Iterativo: {tiempo_iterativo}")
+print(f"Tiempo Recursivo: {tiempo_recursivo}")
+
+
+tiempo_iterativo = timeit.timeit('factorial_iterativo(100)', globals=globals(), number=100)
+tiempo_recursivo = timeit.timeit('factorial_recursivo(100)', globals=globals(), number=100)
+
+print(f"Tiempo Iterativo: {tiempo_iterativo}")
+print(f"Tiempo Recursivo: {tiempo_recursivo}")
